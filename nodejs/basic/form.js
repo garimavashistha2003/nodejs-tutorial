f932d1ed6dc49a879bd67983b0bd72636648c267
Garima vashistha <garima@MacBookAir.lan>
2025-10-28 19:33:38 +0530

    get data in form.js file code

--- nodejs/basic/form.js
+++ nodejs/basic/form.js
@@ -2,6 +2,7 @@
 
 const http = require('http');
 const fs = require('fs');
+const querystring = require('querystring');
 
 http.createServer((req, res) => {
 
@@ -17,6 +18,18 @@ http.createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
     }else if(req.url == '/submit'){
+        
+        let databody = [];
+        req.on('data',(chunk)=>{
+            databody.push(chunk);
+
+        })
+
+        req.on('end',()=>{
+            let rawData = Buffer.concat(databody).toString();
+            const readableData = querystring.parse(rawData);
+            console.log(readableData);
+        })
         res.write('<h1> Data submitted<h1/>')
 
     }
